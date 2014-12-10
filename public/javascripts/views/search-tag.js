6019488c91fb87aaf7b029d904f642b17b509715
Christopher McCulloh <cmcculloh@gmail.com>
2014-12-10 11:56:39 -0500

    made search happen on keyup to not be so annoying to have to click the button, and also made it only search on first word if spaces are present

--- public/javascripts/views/search-tag.js
+++ public/javascripts/views/search-tag.js
@@ -10,22 +10,26 @@ define( [
 		,searchTemplate) {
 	return Marionette.ItemView.extend({
 		template: searchTemplate,
-		
+
 		className: 'repeater-search',
-		
+
 		initialize: function () {
 			this.model = new Backbone.Model({
 				placeholder: 'Search Tags'
 			});
 		},
-		
+
 		events: {
-			'click .btn': 'search'
+			'click .btn': 'search',
+			'keyup input': 'search'
 		},
-		
+
 		search: function () {
 			var $searchInput = this.$('input');
 			var searchText = $searchInput.val();
+			if(searchText.indexOf(' ')){
+				searchText = searchText.substring(0, searchText.indexOf(' '));
+			}
 			this.trigger('search', searchText);
 		}
 	});
